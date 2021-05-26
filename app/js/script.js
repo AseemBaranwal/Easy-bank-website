77f1f4a8efa9014747721bfd2009d022b3b4526f
AseemBaranwal <aseembaranwal23@gmail.com>
2021-05-27 01:24:15 +0530

    Added Website content of Markup styles and Script

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
+
+document.querySelector('#btnHamburger').addEventListener('click', function(){
+    console.log('Click Hamburger');
+    if(header.classList.contains('open')){  //Close hamburger Menu
+        body.classList.remove('no-scroll');
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+    else{
+        body.classList.add('no-scroll');
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+    }
+});
